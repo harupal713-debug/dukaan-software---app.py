71747877fb6442fd8ea0aff5fe46bd3ff3ac7c9c
harupal713-debug <harupal713@gmail.com>
2026-03-01 15:18:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ st.title("SACHIN AATA CHHAKI")
 
 menu = st.selectbox("Kaunsa kaam?", [
     "गेहू खरीदा",
-    "आटा पिसाई (₹2.40/kg)",
+    "आटा पिसाई",
     "आटा सेल",
     "सरसों तेल सेल",
-    "सरसों पिसाई (₹2.40/kg)",
+    "सरसों पिसाई",
     "सरसों खरीद"
 ])
 
@@ -22,21 +22,10 @@ if qty and rate:
         total = qty * rate
         total = round(total, 2)
 
-        st.write("Total Amount: ₹", total)
+        if "खरीद" in menu:
+            st.error(f"देना है: ₹{total}")
+        else:
+            st.success(f"लेना है: ₹{total}")
 
     except:
         st.error("सही नंबर लिखें")
-# Auto calculation
-if "पिसाई" in menu:
-    total = qty * 60
-else:
-    total = qty * rate
-
-# Rounding system
-total = round(total)
-
-# Lena ya Dena
-if "खरीद" in menu:
-    st.error(f"देना है: ₹{total}")
-else:
-    st.success(f"लेना है: ₹{total}")
